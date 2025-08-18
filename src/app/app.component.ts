9a7a86dc159a98e214172b5e38ffa9eb9985929d
Nathan Python <nathan.python@hes-so.ch>
2025-08-18 11:54:19 +0200

    Add server status conditional display

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,17 @@ import { User } from './user.component';
 
 @Component({
   selector: 'app-root',
-  template: `<section><app-user /></section>`,
+  template: `
+    <section>
+      <app-user></app-user>
+
+      @if (isServerRunning) {
+      <p>Yes, the server is running</p>
+      } @else {
+      <p>No, the server is not running</p>
+      }
+    </section>
+  `,
   imports: [User],
   standalone: true,
   styles: [
@@ -16,4 +26,5 @@ import { User } from './user.component';
 })
 export class AppComponent {
   city = 'San Francisco';
+  isServerRunning = true; // Boolean flag to indicate server status
 }
