a50c02a4355e594c055be35dbcfdd9ee8746c3f5
Nathan Python <nathan.python@hes-so.ch>
2025-08-19 15:37:34 +0200

    Separate page-specific content into home and user

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { routes } from './app.routes';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  imports: [BrowserModule, AppComponent],
+  imports: [BrowserModule, AppComponent, RouterModule.forRoot(routes)],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home.component';
+import { User } from './user/user.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', title: 'App Home Page', component: Home },
+  { path: 'user', title: 'App User Page', component: User },
+];
