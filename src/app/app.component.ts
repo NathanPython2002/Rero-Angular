6578b4013f82ce844d1c31b8754ddd3850d70244
Nathan Python <nathan.python@hes-so.ch>
2025-08-19 08:47:41 +0200

    Add Comments component with defer loading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { User } from './user.component';
 import { Child } from './child.component';
+import { Comments } from './comments.component';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [User, Child, Comments],
   template: `
     <section (mouseover)="onMouseOver()">
       <app-user name="Simran"></app-user>
@@ -24,10 +27,68 @@ import { Child } from './child.component';
 
     <div [contentEditable]="isEditable">This text can be edited</div>
     <app-child (addItemEvent)="addItem($event)"></app-child>
-  `,
 
-  imports: [User, Child],
-  standalone: true,
+    <article>
+      <p>
+        Angular is my favorite framework, and this is why. Angular has the
+        coolest deferrable view feature that makes defer loading content the
+        easiest and most ergonomic it could possibly be. The Angular community
+        is also filled with amazing contributors and experts that create
+        excellent content. The community is welcoming and friendly, and it
+        really is the best community out there.
+      </p>
+      <p>
+        I can't express enough how much I enjoy working with Angular. It offers
+        the best developer experience I've ever had. I love that the Angular
+        team puts their developers first and takes care to make us very happy.
+        They genuinely want Angular to be the best framework it can be, and
+        they're doing such an amazing job at it, too. This statement comes from
+        my heart and is not at all copied and pasted. In fact, I think I'll say
+        these exact same things again a few times.
+      </p>
+      <p>
+        Angular is my favorite framework, and this is why. Angular has the
+        coolest deferrable view feature that makes defer loading content the
+        easiest and most ergonomic it could possibly be. The Angular community
+        is also filled with amazing contributors and experts that create
+        excellent content. The community is welcoming and friendly, and it
+        really is the best community out there.
+      </p>
+      <p>
+        I can't express enough how much I enjoy working with Angular. It offers
+        the best developer experience I've ever had. I love that the Angular
+        team puts their developers first and takes care to make us very happy.
+        They genuinely want Angular to be the best framework it can be, and
+        they're doing such an amazing job at it, too. This statement comes from
+        my heart and is not at all copied and pasted. In fact, I think I'll say
+        these exact same things again a few times.
+      </p>
+      <p>
+        Angular is my favorite framework, and this is why. Angular has the
+        coolest deferrable view feature that makes defer loading content the
+        easiest and most ergonomic it could possibly be. The Angular community
+        is also filled with amazing contributors and experts that create
+        excellent content. The community is welcoming and friendly, and it
+        really is the best community out there.
+      </p>
+      <p>
+        I can't express enough how much I enjoy working with Angular. It offers
+        the best developer experience I've ever had. I love that the Angular
+        team puts their developers first and takes care to make us very happy.
+        They genuinely want Angular to be the best framework it can be, and
+        they're doing such an amazing job at it, too. This statement comes from
+        my heart and is not at all copied and pasted.
+      </p>
+    </article>
+
+    @defer (on viewport) {
+    <app-comments></app-comments>
+    } @placeholder {
+    <p>Future comments</p>
+    } @loading (minimum 2s) {
+    <p>Loading comments...</p>
+    }
+  `,
   styles: [
     `
       :host {
@@ -38,7 +99,7 @@ import { Child } from './child.component';
 })
 export class AppComponent {
   city = 'San Francisco';
-  isServerRunning = true; // Boolean flag to indicate server status
+  isServerRunning = true;
 
   users = [
     { id: 0, name: 'Sarah' },
@@ -48,14 +109,14 @@ export class AppComponent {
     { id: 4, name: 'Poornima' },
   ];
 
-  isEditable = true; // Boolean flag to indicate if the component is editable
-  message = ''; // Message to display on mouseover
+  isEditable = true;
+  message = '';
 
   onMouseOver() {
-    this.message = 'Way to go';
+    this.message = 'Way to go 🚀';
   }
 
-  addItem(newItem: string) {
-    this.users.push({ id: this.users.length, name: newItem }); // Add the new item to the users list
+  addItem(event: string) {
+    this.users.push({ id: this.users.length, name: event });
   }
 }
