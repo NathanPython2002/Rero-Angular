eeac924a13382b492f31366f4a3d2c12623448eb
NathanPython2002 <162318284+NathanPython2002@users.noreply.github.com>
2025-08-21 16:56:59 +0200

    feat: add user list with @for in AppComponent (#4)
    * Add User component and conditional rendering in AppComponent
    
    * Add server status conditional display
    
    * feat: add user list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,16 @@ import { User } from './user.component';
       <app-user></app-user>
 
       @if (isServerRunning) {
-        <p>Yes, the server is running</p>
+
+      <p>Yes, the server is running</p>
       } @else {
-        <p>No, the server is not running</p>
+      <p>No, the server is not running</p>
+      }
+
+      <h2>Liste des utilisateurs :</h2>
+      @for (user of users; track user.id) {
+      <p>{{ user.name }}</p>
+
       }
     </section>
   `,
@@ -27,4 +34,14 @@ import { User } from './user.component';
 export class AppComponent {
   city = 'San Francisco';
   isServerRunning = true; // Boolean flag to indicate server status
+
+
+  users = [
+    { id: 0, name: 'Sarah' },
+    { id: 1, name: 'Amy' },
+    { id: 2, name: 'Rachel' },
+    { id: 3, name: 'Jessica' },
+    { id: 4, name: 'Poornima' },
+  ];
+
 }
